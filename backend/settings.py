61a2855307d85214b919319bc51e56c601016735
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 10:56:20 +0800

    Added STATICFILES_DIRS setting pointing to the build/static directory

--- backend/settings.py
+++ backend/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'build/static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
