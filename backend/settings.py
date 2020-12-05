d0705601138ce80343ac8170deec7041fdce9524
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 12:22:59 +0800

    Fixed static files issue from previous commits, turn DEBUG = True to debug server error

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8cbt66kg@!-3l2dgeaqmarjgy6wy0h+7(&+a^2*hd3olh6vj*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['react-dj-todoapp.herokuapp.com', '127.0.0.1:8000', 'localhost']
 
