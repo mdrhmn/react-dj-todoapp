ba8731977e1f0110d648d2e059a79303cb4a9cd3
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 08:07:59 +0800

    Created and migrated Todo model and updated admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
