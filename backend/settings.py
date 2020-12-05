b08b4f88d17205440f693f238eb58ec051bb4bf0
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 16:31:08 +0800

    Remove redundant new line in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
    
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
