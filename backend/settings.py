018c959509bf5244ba1b4eda801f458c8cfc7039
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 14:56:10 +0800

    Dummy change commit

--- backend/settings.py
+++ backend/settings.py
@@ -102,6 +102,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
