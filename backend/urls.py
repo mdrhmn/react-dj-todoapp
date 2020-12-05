58573aea85622037a4e45feb871cc305a3545d9e
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 09:55:53 +0800

    Added missing URL link to React's index.html to Django's urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 from rest_framework import routers
 from todo import views
 
@@ -22,5 +23,7 @@ router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),         path('api/', include(router.urls))
+    path('admin/', admin.site.urls),         
+    path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
