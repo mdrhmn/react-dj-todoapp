eae8b241b487f80c532c9456eb611cca8ddf397e
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2020-12-05 11:40:46 +0800

    Added missing import View library in views.py

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+from django.views import View
 
 # The viewsets base class provides the implementation for CRUD operations by default,
 # what we had to do was specify the serializer class and the query set.
